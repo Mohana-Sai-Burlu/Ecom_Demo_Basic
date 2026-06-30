60fe6e65405f768793a317ce848e6feeaad81fec
Mohana-Sai-Burlu <mohanasaiburlu.github@gmail.com>
2026-06-30 23:34:05 +0530

    inject auth token into localStorage for seamless UI login

--- tests/WebApiPart1.spec.js
+++ tests/WebApiPart1.spec.js
@@ -1,5 +1,6 @@
 const {test, expect, request} = require('@playwright/test');
 const payLoad = {userEmail: "SpiderMan@stark.com", userPassword: "M@rryJa1n"};
+let token;
 
 test.beforeAll( async()=>{
     const ApiContext = await request.newContext();
@@ -11,11 +12,20 @@ test.beforeAll( async()=>{
 
     const jsonResponse = await LoginResponse.json();
 
-    const token = jsonResponse.token;
+    token = jsonResponse.token;
     console.log(token);
 
 });
 
 test("login", async ({page})=>{
 
+    await page.addInitScript(value =>{
+        window.localStorage.setItem('token',value);
+    },token);
+
+    
+    const website ="https://rahulshettyacademy.com/client/#/auth/login";
+     //await page.goto(website);
+
+    
 });
