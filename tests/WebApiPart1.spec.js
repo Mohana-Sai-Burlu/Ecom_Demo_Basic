098e6f2624653f6ba0d0544490a79954712138d1
Mohana-Sai-Burlu <mohanasaiburlu.github@gmail.com>
2026-07-01 23:30:24 +0530

    add API authentication and order validation using Playwright

--- tests/WebApiPart1.spec.js
+++ tests/WebApiPart1.spec.js
@@ -1,9 +1,15 @@
 const {test, expect, request} = require('@playwright/test');
 const payLoad = {userEmail: "SpiderMan@stark.com", userPassword: "M@rryJa1n"};
 let token;
+let orderID;
+
+const orderPayLoad = {orders: [{country: "cuba", productOrderedId: "6960ea76c941646b7a8b3dd5"}]};
+
 
 test.beforeAll( async()=>{
     const ApiContext = await request.newContext();
+
+    //Login
     const LoginResponse = await ApiContext.post("https://rahulshettyacademy.com/api/ecom/auth/login",
     {
         data:payLoad
@@ -15,6 +21,21 @@ test.beforeAll( async()=>{
     token = jsonResponse.token;
     console.log(token);
 
+     //OrderApi
+   const orderResponse= await ApiContext.post("https://rahulshettyacademy.com/api/ecom/order/create-order",{
+         data:orderPayLoad,
+
+         headers:{
+            "authorization":token,
+            "content-type":'application/json'
+         },
+
+    });
+
+    const orderJsonResponse = await orderResponse.json();
+    orderID = orderJsonResponse.orders[0];
+
+
 });
 
 test("login", async ({page})=>{
@@ -25,7 +46,21 @@ test("login", async ({page})=>{
 
     
     const website ="https://rahulshettyacademy.com/client/#/auth/login";
-     //await page.goto(website);
+    await page.goto(website);
+
+    await page.getByRole("button",{name:'ORDERS'}).click();
+    const count = await page.locator("//tbody/tr").filter({hasText:orderID}).getByRole("button",{name:"view"}).click();
+    const eleorderID = await page.locator('//*[@class="col-text -main"]').textContent();
+    await expect(orderID).toEqual(eleorderID);
+    
+
+
+    //await page.pause();
+
+
+
+
+
 
     
 });
