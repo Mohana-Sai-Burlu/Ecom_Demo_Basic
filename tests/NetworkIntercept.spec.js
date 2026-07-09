a326312369f67e1637eef42436010f86bb2744ef
Mohana-Sai-Burlu <mohanasaiburlu.github@gmail.com>
2026-07-09 23:33:30 +0530

    Learned Playwright API response mocking and route interception

---
+++ tests/NetworkIntercept.spec.js
@@ -0,0 +1,45 @@
+const {test, expect, request} = require('@playwright/test');
+const {ApiUtiles} = require('./Utiles/ApiUtiles');
+const payLoad = {userEmail: "SpiderMan@stark.com", userPassword: "M@rryJa1n"};
+const fakePayLoadOrders = { data: [], message: "No Orders" };
+let response;
+const orderPayLoad = {orders: [{country: "cuba", productOrderedId: "6960ea76c941646b7a8b3dd5"}]};
+test.beforeAll(async () => {
+    const ApiContext = await request.newContext();
+
+    const apiutiles = new ApiUtiles(ApiContext, payLoad);
+
+    response = await apiutiles.createOrderID(orderPayLoad);
+})
+ 
+ 
+//create order is success
+test('@SP Place the order', async ({ page }) => {
+  page.addInitScript(value => {
+ 
+    window.localStorage.setItem('token', value);
+  }, response.token);
+  await page.goto("https://rahulshettyacademy.com/client");
+ 
+ 
+  await page.route("https://rahulshettyacademy.com/api/ecom/order/get-orders-for-customer/*",
+    async route => {
+      const response = await page.request.fetch(route.request());
+      let body = JSON.stringify(fakePayLoadOrders);
+      route.fulfill(
+        {
+          response,
+          body, 
+ 
+        });
+      //intercepting response -APi response-> { playwright fakeresponse}->browser->render data on front end
+    });
+ 
+  await page.locator("button[routerlink*='myorders']").click();
+  await page.waitForResponse("https://rahulshettyacademy.com/api/ecom/order/get-orders-for-customer/*")
+ 
+  console.log(await page.locator(".mt-4").textContent());
+ 
+ 
+ 
+});
